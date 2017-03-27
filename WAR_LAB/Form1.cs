b1fa33e7b1812f624fa9628988323843d7815f4b
Fejcvk <tomaszfejcak@icloud.com>
2017-03-28 00:15:53 +0200

    Changes : - number images - fully implemented game logic - basically full view of main window

--- WAR_LAB/Form1.cs
+++ WAR_LAB/Form1.cs
@@ -13,6 +13,7 @@ namespace WAR_LAB
 {
     public partial class Form1 : Form
     {
+        //some global variables
         readonly Random random = new Random();
         int _roundCounter = 0;
         int _playerScore = 10;
@@ -26,6 +27,10 @@ namespace WAR_LAB
         private int _cardAmount = 0;
         private Queue<int> PlayerSet = new Queue<int>();
         private Queue<int> OpponentSet = new Queue<int>();
+        private List<int> GameDeck = new List<int>();
+
+
+        //whole behaviour of setup box
         public void GameSetupBox()
         {
             var setupBox = new Form2();
@@ -66,21 +71,34 @@ namespace WAR_LAB
             InitializeComponent();
             GameSetupBox();
         }
-
+        //function fill main deck and then divide cards into players queues
         public void FillDecksWithCards(Queue<int> queue, Queue<int> queue2)
         {
-            for (int i = 0; i < _cardAmount / 2 ; i++)
+            //fill list main deck with cards 1-10 * number of sets
+            for (int i = 0; i < _setAmount ; i++)
             {
-                queue.Enqueue(random.Next(1, 11));
-                queue2.Enqueue(random.Next(1, 11));
+                for (int j = 1; j < 11; j++)
+                {
+                    GameDeck.Add(j);
+                }
+            }
+            //while list containing all set of cards is non empty pick two random indices
+            //and enqueue them into the players queue's
+            while (GameDeck.Count != 0)
+            {
+                int randomixd1 = random.Next(0, GameDeck.Count);
+                int randomixd2 = random.Next(0, GameDeck.Count-1);
+                queue.Enqueue(GameDeck.ElementAt(randomixd1));
+                GameDeck.RemoveAt(randomixd1);
+                queue2.Enqueue(GameDeck.ElementAt(randomixd2));
+                GameDeck.RemoveAt(randomixd2);
             }
         }
+        //setup first game
         private void FirstGame(string _PlayerName, string _OppName, int Rounds)
         {
             PlayerNameLabel.Text = _PlayerName;
             OpponentName.Text = _OppName;
-            PlayerButton.Text = _PlayerName + " cards";
-            OponnentButton.Text = _OppName + " cards";
             PlayerScoreTag.Text = _PlayerName + " points ";
             OpponentScoreTag.Text = _OppName + " points ";
             _maxRounds = Rounds;
@@ -96,8 +114,10 @@ namespace WAR_LAB
             PlayerScoreButton.BackColor = System.Drawing.Color.Empty;
             OponnentScoreButton.BackColor = System.Drawing.Color.Empty;
             FillDecksWithCards(PlayerSet, OpponentSet);
-        }
+            PlayerScoreButton.BackgroundImage = OponnentScoreButton.BackgroundImage = null;
 
+        }
+        //setup newgame
         private void NewGame()
         {
             _roundCounter = 0;
@@ -112,13 +132,21 @@ namespace WAR_LAB
             PlayerScoreButton.BackColor = System.Drawing.Color.Empty;
             OponnentScoreButton.BackColor = System.Drawing.Color.Empty;
             FillDecksWithCards(PlayerSet, OpponentSet);
-        }
+            PlayerScoreButton.BackgroundImage = OponnentScoreButton.BackgroundImage = null;
 
+        }
+        //setup new _maxRound baased on input
         public string getLastRound()
         {
             return LastRoundTextbox.Text;
         }
-
+        //function is called while Start button is pushed
+        private void setRoundForAutoplay()
+        {
+            int newMaxRound = int.Parse(getLastRound());
+            _maxRounds = newMaxRound;
+        }
+        //dropdown menu
         private void toolStripButton1_Click(object sender, EventArgs e)
         {
             string message = "War v.1.0";
@@ -135,11 +163,9 @@ namespace WAR_LAB
                 return;
             }
         }
-
+        //player won round
         private void PlayerWonRound(int playerScore, int _cpuScore)
         {
-            PlayerScoreButton.Text = playerScore.ToString();
-            OponnentScoreButton.Text = _cpuScore.ToString();
             _roundCounter += 1;
             _playerScore += 1;
             _opponentScore -= 1;
@@ -148,11 +174,9 @@ namespace WAR_LAB
             PlayerSet.Enqueue(playerScore);
             PlayerSet.Enqueue(_cpuScore);
         }
-
+        //cpu won round
         private void CpuWonRound(int playerScore, int _cpuScore)
         {
-            PlayerScoreButton.Text = playerScore.ToString();
-            OponnentScoreButton.Text = _cpuScore.ToString();
             _roundCounter += 1;
             _opponentScore += 1;
             _playerScore -= 1;
@@ -161,54 +185,53 @@ namespace WAR_LAB
             OpponentSet.Enqueue(playerScore);
             OpponentSet.Enqueue(_cpuScore);
         }
-
-        private void RestoreButtonViewAfterWar()
-        {
-            //PlayerScoreButton.BackgroundImage = nowy numerek
-            //OponnentScoreButton.BackgroundImage = nowy numerek
-        }
+        //tie situation solution
         private void Tie()
         {
             PlayerSet.Dequeue();
             OpponentSet.Dequeue();
             War();
-            if (_warIsOver == true)
-            {
-                RestoreButtonViewAfterWar();
-            }
-            _warIsOver = false;
         }
-
+        //war situation solution
         private void War()
         {
             PlayerScoreButton.BackgroundImage = PlayerButton.BackgroundImage;
             OponnentScoreButton.BackgroundImage = PlayerButton.BackgroundImage;
             int _pScore = PlayerSet.Dequeue();
             int _oScore = OpponentSet.Dequeue();
-            if (_pScore > _oScore)
-            {
-                PlayerWonRound(_playerScore, _oScore);
-                _warIsOver = true;
-            }
-            else if (_pScore < _oScore)
-            {
-                CpuWonRound(_pScore, _oScore);
-                _warIsOver = true;
-            }
-            else
-            {
-                Tie();
-            }
         }
+        //set right png to given value
+        private void SetNumberPicture(Button button, int index)
+        {
+            Rectangle section = new Rectangle(100 * (index - 1), 0, 100, 147);
+
+            Bitmap src = Properties.Resources.numerki1;
+            Bitmap target = new Bitmap(section.Width, section.Height);
+
+            using (Graphics g = Graphics.FromImage(target))
+            {
+                g.DrawImage(src, new Rectangle(0, 0, target.Width, target.Height), section, GraphicsUnit.Pixel);
+            }
+
+            button.BackgroundImage = target;
+            button.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+        //dropdown menu
         private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
         {
             GameSetupBox();
         }
-
-        private void GameEngine(Queue<int> PlayerDeck, Queue<int>CpuDeck)
+        //game logic
+        private void GameEngine(Queue<int> playerDeck, Queue<int>cpuDeck)
         {
-            int PlayerScoreValue = PlayerDeck.Dequeue();
-            int OponnentScoreValue = CpuDeck.Dequeue();
+            //get player number out of queue
+            int PlayerScoreValue = playerDeck.Dequeue();
+            //set right png
+            SetNumberPicture(PlayerScoreButton, PlayerScoreValue);
+            //get cpu number out of queue
+            int OponnentScoreValue = cpuDeck.Dequeue();
+            //set right png
+            SetNumberPicture(OponnentScoreButton, OponnentScoreValue);
             //player has bigger number
             if (PlayerScoreValue > OponnentScoreValue)
             {
@@ -287,7 +310,7 @@ namespace WAR_LAB
             PlayerScoreLabel.Text = _playerScore.ToString();
             OponnentScoreLabel.Text = _opponentScore.ToString();
         }
-
+        //player button behaviour
         private void PlayerController_Click(object sender, EventArgs e)
         {
            GameEngine(PlayerSet, OpponentSet);
@@ -315,4 +338,4 @@ namespace WAR_LAB
             }
         }
     }
-}
+} 
